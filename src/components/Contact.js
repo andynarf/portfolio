3e7780dd548249dcccc0ddcc9da204803acd7078
Andres Rodriguez <andresrodriguez4@outlook.com.ar>
2020-09-15 17:09:52 -0300

    added more social media, fixed colors

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,28 +1,80 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import './contact.css'
-
+import React from "react";
+import Container from "react-bootstrap/Container";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./contact.css";
 
 export default function Contact() {
-    return (
-        <Container className='contact-container'>
-            <h2> <a id='Contact' href='#Contact' className='section-title touch'>Get In Touch  </a></h2>
-            <p className='contact-text'>To get in touch with me, you can send me an email, or contact me via GitHub or LinkedIn.</p>
+  return (
+    <Container className="contact-container">
+      <h2>
+        {" "}
+        <a id="Contact" href="#Contact" className="section-title touch">
+          Get In Touch{" "}
+        </a>
+      </h2>
+      <p className="contact-text">
+        To get in touch with me, you can send me an email, or contact me via
+        GitHub or LinkedIn.
+      </p>
 
-            <h3 className='mail-container'>
-                <FontAwesomeIcon className='icon' icon={'envelope'} />
-                <a className='myemail' href="mailto:andresrodriguez4@outlook.com.ar">andresrodriguez4@outlook.com.ar</a>
-            </h3>
-            <h3 className='myemail'>
-                <FontAwesomeIcon className='icon' icon={'mobile-alt'} />
-            (+54) 11-6019-0598
-            </h3>
-            <div>
-                <a target='_blank' rel="noopener noreferrer" href='https://github.com/andynarf'> <FontAwesomeIcon size='3x' icon={['fab', 'github']} className='contact-icon' /></a>
-                <a target='_blank' rel="noopener noreferrer" href='https://www.linkedin.com/in/andres-dev/'>   <FontAwesomeIcon size='3x' icon={['fab', 'linkedin']} className='contact-icon' /></a>
-            </div>
-
-        </Container>
-    )
+      <h3 className="mail-container">
+        <FontAwesomeIcon className="icon" icon={"envelope"} />
+        <a className="myemail" href="mailto:andresrodriguez4@outlook.com.ar">
+          andresrodriguez4@outlook.com.ar
+        </a>
+      </h3>
+      <h3 className="myemail">
+        <FontAwesomeIcon className="icon" icon={"mobile-alt"} />
+        (+54) 11-6019-0598
+      </h3>
+      <div>
+      <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://twitter.com/untalandy"
+        >
+          {" "}
+          <FontAwesomeIcon
+            size="2x"
+            icon={["fab", "twitter"]}
+            className="contact-icon"
+          />
+        </a>        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://angel.co/u/carlos-andres-rodriguez"
+        >
+          {" "}
+          <FontAwesomeIcon
+            size="2x"
+            icon={["fab", "angellist"]}
+            className="contact-icon"
+          />
+        </a>        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/andynarf"
+        >
+          {" "}
+          <FontAwesomeIcon
+            size="2x"
+            icon={["fab", "github"]}
+            className="contact-icon"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/andres-dev/"
+        >
+          {" "}
+          <FontAwesomeIcon
+            size="2x"
+            icon={["fab", "linkedin"]}
+            className="contact-icon"
+          />
+        </a>
+      </div>
+    </Container>
+  );
 }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,47 +1,79 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
-import './intro.css'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import "./intro.css";
 
 export default function Intro() {
-    return (
-        <div className='intro'>
-            <Row>
-                <Col md={2}></Col>
-                <Col md={10}>
-                    <h1 className='no-centered my-name'>Andres Rodriguez</h1>
-                    <h5 className='no-centered subtitle'>Software Developer</h5>
-                </Col></Row>
-            <Row>
-                <Col md={2}></Col>
+  return (
+    <div className="intro">
+      <Row>
+        <Col md={2}></Col>
+        <Col md={10}>
+          <h1 className="no-centered my-name">Andres Rodriguez</h1>
+          <h5 className="no-centered subtitle">Software Developer</h5>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={2}></Col>
 
-                <Col md={6}>
-                    <p className='no-centered'>
-                        Full stack developer and former
-                        Itamae (Traditional Japanese chef)
-                        hoping to combine my passion for
-                        building responsive apps and designs
-                        with my education, pair-programming
-                        experience, and precision knife skills.
-                        <br />
-                        Just kidding on the knife skills. :)
-            </p>
-                </Col>
-            </Row>
-            <Row className='social-container'>
-                <Col md={2}></Col>
-                <Col className='align-right social-media-back' md={6}>
-                    <p className='media-text'>
-                        My works in social media networks
-                    </p>
-                    <div>
-                        <a target='_blank' className='azul-oscuro' rel="noopener noreferrer" href='https://www.linkedin.com/in/andres-dev/'>   <FontAwesomeIcon size='2x' icon={['fab', 'linkedin']} /></a>
-                        <a target='_blank' className='azul-oscuro' rel="noopener noreferrer" href='https://github.com/andynarf'> <FontAwesomeIcon size='2x' icon={['fab', 'github']} /></a>
-                    </div>
-                </Col>
-                <Col md={4}></Col>
-            </Row>
-        </div>
-    )
+        <Col md={6}>
+          <p className="no-centered">
+            Full stack developer and former Itamae (Traditional Japanese chef)
+            hoping to combine my passion for building responsive apps and
+            designs with my education, pair-programming experience, and
+            precision knife skills.
+            <br />
+            Just kidding on the knife skills. :)
+          </p>
+        </Col>
+      </Row>
+      <Row className="social-container">
+        <Col md={2}></Col>
+        <Col className="align-right social-media-back" md={6}>
+          <p className="media-text">Contact me on Social Media</p>
+          <div className="media-icons">
+            <a
+              target="_blank"
+              className="azul-oscuro"
+              rel="noopener noreferrer"
+              href="https://twitter.com/untalandy"
+            >
+              {" "}
+              <FontAwesomeIcon size="2x" icon={["fab", "twitter"]} />
+            </a>
+            <a
+              target="_blank"
+              className="azul-oscuro"
+              rel="noopener noreferrer"
+              href="https://angel.co/u/carlos-andres-rodriguez"
+            >
+              {" "}
+              <FontAwesomeIcon size="2x" icon={["fab", "angellist"]} />
+            </a>
+
+            <a
+              target="_blank"
+              className="azul-oscuro"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/andres-dev/"
+            >
+              {" "}
+              <FontAwesomeIcon size="2x" icon={["fab", "linkedin"]} />
+            </a>
+            <a
+              target="_blank"
+              className="azul-oscuro"
+              rel="noopener noreferrer"
+              href="https://github.com/andynarf"
+            >
+              {" "}
+              <FontAwesomeIcon size="2x" icon={["fab", "github"]} />
+            </a>
+          </div>
+        </Col>
+        <Col md={4}></Col>
+      </Row>
+    </div>
+  );
 }
