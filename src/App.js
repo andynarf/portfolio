2c6c2d269266a1d710ed94e4681e4ab25069a8c4
Andres Rodriguez <andresrodriguez4@outlook.com.ar>
2020-09-16 17:50:47 -0300

    added react-helmet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import './App.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faMobileAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import NavBar from './components/NavBar'
-import Intro from './components/Intro'
-import PortFolio from './components/PortFolio'
-import About from './components/About'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Contact from './components/Contact'
-import Normalize from 'react-normalize'
+import React from "react";
+import { Helmet } from "react-helmet";
+import "./App.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faMobileAlt, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import NavBar from "./components/NavBar";
+import Intro from "./components/Intro";
+import PortFolio from "./components/PortFolio";
+import About from "./components/About";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Contact from "./components/Contact";
+import Normalize from "react-normalize";
 
-library.add(fab, faMobileAlt, faEnvelope)
-
+library.add(fab, faMobileAlt, faEnvelope);
 
 function App() {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Portfolio</title>
+        <link rel="canonical" href="./demo.png" />
+      </Helmet>
       <Normalize></Normalize>
       <Container fluid className="App">
         <Row>
@@ -27,17 +32,16 @@ function App() {
           </Col>
           <Col md={10} className="right-side">
             <Intro></Intro>
-            <PortFolio ></PortFolio>
-            <div className='about-contact'>
-              <About ></About>
-              <Contact ></Contact>
+            <PortFolio></PortFolio>
+            <div className="about-contact">
+              <About></About>
+              <Contact></Contact>
             </div>
           </Col>
         </Row>
-
       </Container>
     </>
-  )
+  );
 }
 
 export default App;
