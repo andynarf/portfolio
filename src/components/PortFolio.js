c577ca4f675f5742fa25f79455f1b052f860324e
Andres Rodriguez <andresrodriguez4@outlook.com.ar>
2020-09-23 20:27:43 -0300

    updated projects

--- src/components/PortFolio.js
+++ src/components/PortFolio.js
@@ -33,7 +33,7 @@ export default function PortFolio() {
             image: tictactoe,
             text: 'A Tic Tac Toe game made on Ruby.'
         }, {
-            name: 'Hulo UI clone',
+            name: 'Hulu UI clone',
             live: 'https://hulu-ui-clone.netlify.app/',
             github: 'https://github.com/andynarf/hulu_clone',
             image: hulu,
